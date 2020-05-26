5859c640dff46464d93cd7dd8e0eeb05dcf3c5f5
Gregory Reda <gregoryreda@Gregorys-MacBook-Pro-2.local>
2020-05-26 11:51:19 -0400

    Mapview update

--- src/components/ArcGIS/Mapview.js
+++ src/components/ArcGIS/Mapview.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
-import {Grid, Cell} from 'react-mdl';
 import { WebMap, WebScene } from '@esri/react-arcgis';
 
 class MapView extends Component {
     render(){
         return(
-            <div style={{ width: '70vw', height: '50vh' }}>
+            <div style={{ width: '80vw', height: '80vh' }}>
                 <h1>NJ Influenza Deaths 2014-2018</h1>
             <WebMap id="b3330cfb0f38447f8613273772327c37" />
             </div>
