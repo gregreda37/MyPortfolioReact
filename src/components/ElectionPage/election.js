9f1365192ed758706d186712c12dba52d1bd851e
Gregory Reda <gregoryreda@MacBook-Pro.local>
2020-11-17 15:35:47 -0500

    Data vis

--- src/components/ElectionPage/election.js
+++ src/components/ElectionPage/election.js
@@ -1,33 +1,26 @@
 import React, { Component } from 'react'
 import TableauReport from 'tableau-react';
  
-const SimpleReport = props => (
-  <TableauReport
-    url="https://prod-useast-b.online.tableau.com/t/myportfoliosite/views/FinalExamDATA/Sheet3?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link"
-
-  />
-)
-
 class Election extends Component {
     render() {
         return (
             <>
               <TableauReport
-                url="https://prod-useast-b.online.tableau.com/t/myportfoliosite/views/AgeTreeMapFinal/Sheet1?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link"
+                url="https://public.tableau.com/views/AgeTreeMapFinal/Sheet1?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
             />
               <TableauReport
-                url="https://prod-useast-b.online.tableau.com/t/myportfoliosite/views/FinalExamDATA/Sheet1?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link"
+                url="https://public.tableau.com/views/FinalExamDATA/Sheet1?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
             />
               <TableauReport
-                url="https://prod-useast-b.online.tableau.com/t/myportfoliosite/views/FinalExamDATADotChart/Sheet2?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link"
+                url="https://public.tableau.com/views/FinalExamDATA/Sheet2?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
             />
               <TableauReport
-                url="https://prod-useast-b.online.tableau.com/t/myportfoliosite/views/FinalExamDATADotChart/Sheet3?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link"
+                url="https://public.tableau.com/shared/B2NMN4334?:display_count=y&:origin=viz_share_link"
             />
 
 
                           <TableauReport
-                url="https://prod-useast-b.online.tableau.com/t/myportfoliosite/views/FinalExamDATABubbleMap/Sheet4?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link"
+                url="https://public.tableau.com/views/FinalExamDATA/Sheet4?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
             />
 
             </>
